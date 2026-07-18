eb752edfc60f49e79575cb8ce029c1bf25247f78
Manthan Domde <dmanthan67@gmail.com>
2026-07-18 14:34:01 +0530

    Prepare backend for deployment

--- server/config/db.js
+++ server/config/db.js
@@ -2,15 +2,15 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("✅ MongoDB Connected");
 
   } catch (error) {
 
-    console.log(error);
+    console.error("❌ MongoDB Connection Error:", error.message);
 
+    process.exit(1);
   }
 };
 
--- server/server.js
+++ server/server.js
@@ -7,9 +7,6 @@ import contactRoutes from "./routes/contactRoutes.js";
 
 dotenv.config();
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "Loaded ✅" : "Not Loaded ❌");
-
 connectDB();
 
 const app = express();
